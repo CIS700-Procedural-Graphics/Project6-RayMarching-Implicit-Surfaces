7eeec3cd51d0860f3e03d0cac6459beadb81be3f
Austin Eng <213reeses@gmail.com>
2016-12-16 12:10:07 -0500

    create folder for website content

--- tools/targets.js
+++ tools/targets.js
@@ -1,4 +1,8 @@
 module.exports = {
+  website: {
+    directory: "website",
+    repo: "git@github.com:CIS700-Procedural-Graphics/cis700-procedural-graphics.github.io.git"
+  },
   hw01_base: {
     directory: "hw/hw01/base",
     repo: "git@github.com:CIS700-Procedural-Graphics/HW1-Noise.git",
