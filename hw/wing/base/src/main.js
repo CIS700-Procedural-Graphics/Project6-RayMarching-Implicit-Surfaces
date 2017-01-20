b19c0fde139763ec580d002df3ff1866f12375e7
Austin Eng <213reeses@gmail.com>
2017-01-19 19:59:49 -0500

    different publish system. add project 2

---
+++ hw/wing/base/src/main.js
@@ -0,0 +1,73 @@
+
+// Skybox texture from: https://github.com/mrdoob/three.js/tree/master/examples/textures/cube/skybox
+
+const THREE = require('three'); // older modules are imported like this. You shouldn't have to worry about this much
+import Framework from './framework'
+
+// called after the scene loads
+function onLoad(framework) {
+    var scene = framework.scene;
+    var camera = framework.camera;
+    var renderer = framework.renderer;
+    var gui = framework.gui;
+    var stats = framework.stats;
+
+    // Basic Lambert white
+    var lambertWhite = new THREE.MeshLambertMaterial({ color: 0xaaaaaa, side: THREE.DoubleSide });
+
+    // Set light
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    directionalLight.color.setHSL(0.1, 1, 0.95);
+    directionalLight.position.set(1, 3, 2);
+    directionalLight.position.multiplyScalar(10);
+
+    // set skybox
+    var loader = new THREE.CubeTextureLoader();
+    var urlPrefix = '/images/skymap/';
+
+    var skymap = new THREE.CubeTextureLoader().load([
+        urlPrefix + 'px.jpg', urlPrefix + 'nx.jpg',
+        urlPrefix + 'py.jpg', urlPrefix + 'ny.jpg',
+        urlPrefix + 'pz.jpg', urlPrefix + 'nz.jpg'
+    ] );
+
+    scene.background = skymap;
+
+    // load a simple obj mesh
+    var objLoader = new THREE.OBJLoader();
+    objLoader.load('/geo/feather.obj', function(obj) {
+
+        // LOOK: This function runs after the obj has finished loading
+        var featherGeo = obj.children[0].geometry;
+
+        var featherMesh = new THREE.Mesh(featherGeo, lambertWhite);
+        featherMesh.name = "feather";
+        scene.add(featherMesh);
+    });
+
+    // set camera position
+    camera.position.set(0, 1, 5);
+    camera.lookAt(new THREE.Vector3(0,0,0));
+
+    // scene.add(lambertCube);
+    scene.add(directionalLight);
+
+    // edit params and listen to changes like this
+    // more information here: https://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage
+    gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
+        camera.updateProjectionMatrix();
+    });
+}
+
+// called on frame updates
+function onUpdate(framework) {
+    var feather = framework.scene.getObjectByName("feather");    
+    if (feather !== undefined) {
+        // Simply flap wing
+        var date = new Date();
+        feather.rotateZ(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180);        
+    }
+}
+
+// when the scene is done initializing, it will call onLoad, then on frame updates, call onUpdate
+Framework.init(onLoad, onUpdate);
