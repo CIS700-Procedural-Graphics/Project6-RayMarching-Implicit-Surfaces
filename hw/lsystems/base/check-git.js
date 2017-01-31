3439fd4b01aa22892e01c3093a50c5496bf1bf02
Austin Eng <213reeses@gmail.com>
2017-01-31 17:26:07 -0500

    add git status check and gitignore

---
+++ hw/lsystems/base/check-git.js
@@ -0,0 +1,35 @@
+var colors = require('colors');
+var path = require('path');
+var git = require('simple-git')(path.join(__dirname, '..'));
+
+var success = 1;
+git.fetch('origin', 'master', function(err) {
+    if (err) throw err;
+    git.status(function(err, status) {
+        if (err) throw err;
+        if (!status.isClean()) {
+            success = 0;
+            console.error('Error: You have uncommitted changes! Please commit them first'.red);   
+        }
+
+        if (status.current !== 'master') {
+            success = 0;
+            console.warn('Warning: Please deploy from the master branch!'.yellow)
+        }
+
+        git.diffSummary(['origin/master'], function(err, diff) {
+            if (err) throw err;
+
+            if (diff.files.length || diff.insertions || diff.deletions) {
+                success = 0;
+                console.error('Error: Current branch is different from origin/master! Please push all changes first'.red)
+            }
+
+            if (!success) {
+                throw new Error('git requirements for deploy failed!')
+            }
+        })
+
+        
+    })
+})
