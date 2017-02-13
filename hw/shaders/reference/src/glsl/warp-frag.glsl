b4492995c3ef60fabd6d487e78859fff000f1aa1
Austin Eng <213reeses@gmail.com>
2017-02-12 23:33:34 -0500

    different noise functions for x,y

--- hw/shaders/reference/src/glsl/warp-frag.glsl
+++ hw/shaders/reference/src/glsl/warp-frag.glsl
@@ -11,6 +11,10 @@ uniform float u_period;
 float hash(float n) { return fract(sin(n) * 1e4); }
 float hash(vec2 p) { return fract(1e4 * sin(17.0 * p.x + p.y * 0.1) * (0.1 + abs(sin(p.y * 13.0 + p.x)))); }
 
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
 float noise(float x) {
     float i = floor(x);
     float f = fract(x);
@@ -18,7 +22,7 @@ float noise(float x) {
     return mix(hash(i), hash(i + 1.0), u);
 }
 
-float noise(vec2 x) {
+float noise1(vec2 x) {
     vec2 i = floor(x);
     vec2 f = fract(x);
 
@@ -39,8 +43,29 @@ float noise(vec2 x) {
     return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
 }
 
+float noise2(vec2 x) {
+    vec2 i = floor(x);
+    vec2 f = fract(x);
+
+    // Four corners in 2D of a tile
+    float a = rand(i);
+    float b = rand(i + vec2(1.0, 0.0));
+    float c = rand(i + vec2(0.0, 1.0));
+    float d = rand(i + vec2(1.0, 1.0));
+
+    // Simple 2D lerp using smoothstep envelope between the values.
+    // return vec3(mix(mix(a, b, smoothstep(0.0, 1.0, f.x)),
+    //          mix(c, d, smoothstep(0.0, 1.0, f.x)),
+    //          smoothstep(0.0, 1.0, f.y)));
+
+    // Same code, with the clamps in smoothstep and common subexpressions
+    // optimized away.
+    vec2 u = f * f * (3.0 - 2.0 * f);
+    return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
+
 vec2 distort(vec2 uv) {
-    vec2 offset = vec2(noise(uv * u_resolution.x / u_period), noise((uv + 123.456) * u_resolution.y / u_period));
+    vec2 offset = vec2(noise1(uv * u_resolution.x / u_period), noise2(uv * u_resolution.y / u_period));
     return uv + u_amplitude * (offset - 0.5) / u_resolution;
 }
 
